03dcf7aa4fe260387361d9ba18f23f5562da910b
cre8ivesolutions <tammerz78@gmail.com>
2022-11-02 18:44:54 -0500

    done

--- python_setup.py
+++ python_setup.py
@@ -6,10 +6,16 @@ def hello():
 def pack(one, two, three):
     return (one, two, three)
 
-def eat_lunch(breakfast, *lunch):
-    if breakfast == "":
+def eat_lunch(foods):
+    if len(foods) == 0:
         print ("My lunchbox is empty!")
     else:
-        print ("First I eat " + breakfast + ". Next I eat " + lunch)
+       for i in range(len(my_lst)):
+       if i == 0:
+        print(f"First I eat {foods[0]}")
+      else:
+        print(f"Next I eat {foods[i]}")
     
+hello()
 print(pack(1, 2, 3))
+eat_lunch(["eggs", "pizza"])
