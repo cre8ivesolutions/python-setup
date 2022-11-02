1ad90f88774534a2897347db2bb6734d76a728a3
cre8ivesolutions <tammerz78@gmail.com>
2022-11-02 18:53:28 -0500

    finally done

---
+++ functions_practice.py
@@ -0,0 +1,19 @@
+def hello():
+    print("hello user")
+
+def pack(one, two, three):
+    return (one, two, three)
+
+def eat_lunch(foods):
+    if len(foods) == 0:
+        print("My lunchbox is empty!")
+    else:
+       for i in range(len(foods)):
+        if i == 0:
+            print(f"First I eat {foods[0]}")
+        else:
+            print(f"Next I eat {foods[i]}")
+    
+hello()
+print(pack(1, 2, 3))
+eat_lunch(["eggs", "pizza"])
